569668bccda146ba84dc2d878127fc2d7345b8f4
sanykos <kosih2009@yandex.ru>
2020-08-23 17:21:17 +0300

    use effect

--- src/pages/authentication/index.js
+++ src/pages/authentication/index.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 
 
 const Authentication = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+   // const emailRef = useRef(null)
+   // console.log(emailRef)
+   // console.log(email)
+   useEffect(() => {
+       console.log('dsadsad')
+   }, [email])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('data', email, password)
+    }
+
     return(
         <div className="auth-page">
             <div className="container page">
@@ -13,19 +29,25 @@ const Authentication = () => {
                         <p className="text-center">
                             <Link to="register">Need an account</Link>
                         </p>
-                        <form action="">
+                        <form onSubmit={handleSubmit}>
                             <fieldset>
                                 <fieldset className="form-group">
                                     <input 
                                     type="email" 
                                     className="form-control form-control-lg" 
-                                    placeholder="Email" />
+                                    placeholder="Email" 
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    />
                                 </fieldset>
                                 <fieldset className="form-group">
                                     <input 
                                     type="password" 
                                     className="form-control form-control-lg" 
-                                    placeholder="Password" />
+                                    placeholder="Password" 
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    />
                                 </fieldset>
                                 <button className="btn btn-lg btn-primary" type="submit">Sign in</button>
                             </fieldset>
